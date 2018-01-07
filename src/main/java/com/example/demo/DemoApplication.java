558de81743b767d78ef87d258a3a91fb21dc4f81
abhishek <abhishek@abhishek-Aspire-5742>
2018-01-06 22:39:46 -0500

    new code changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -3,21 +3,27 @@ package com.example.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.dao.UserDaoImpl;
 import com.example.demo.model.User;
 
 @Controller
 public class FormController {
 
+	@Autowired
+	UserDaoImpl userDao;
+	
 	@RequestMapping("/details")
 	public ModelAndView submitdetails(HttpServletRequest request,HttpServletResponse response,@ModelAttribute("user") User user){
 		
 		System.out.println(user.getProvFirstName());
 		System.out.println(user.getProvLastName());
+		userDao.addUser(user);
 		return new ModelAndView("index");
 	}
 }
